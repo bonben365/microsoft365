f60689526a28d96a0ce61fd509398c9c908030c0
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-26 13:49:40 +0700

    Update ELCLastSuccessTimestamp.ps1

--- ExchangeOnline/ELCLastSuccessTimestamp.ps1
+++ ExchangeOnline/ELCLastSuccessTimestamp.ps1
@@ -1,1 +1,23 @@
+$mailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited
+$report = @()
+$i = 0
+ForEach ($mailbox in $mailboxes) {
+    $i++
+    $LastProcessed = $Null
+    Write-Progress -Activity "Scanning Mailbox $($mailbox.DisplayName)" -Status "Scanned: $i of $($mailboxes.Count)"
+    $Log = Export-MailboxDiagnosticLogs -Identity $mailbox.UserPrincipalName -ExtendedProperties
+    $xml = [xml]($Log.MailboxLog)  
+    $LastProcessed = ($xml.Properties.MailboxTable.Property | ? {$_.Name -like "*ELCLastSuccessTimestamp*"}).Value   
+    $ItemsDeleted  = $xml.Properties.MailboxTable.Property | ? {$_.Name -like "*ElcLastRunDeletedFromRootItemCount*"}
+    If ($LastProcessed -eq $Null) {
+        $LastProcessed = "Not processed"}
 
+    $reportLine = [PSCustomObject]@{
+            User          = $mailbox.DisplayName
+            LastProcessed = $LastProcessed
+            ItemsDeleted  = $ItemsDeleted.Value}      
+        $report += $reportLine
+    }
+#$report | Select User, LastProcessed, ItemsDeleted
+#$report | Export-CSV C:\Scripts\Inboxsizes.csv
+$report | Out-GridView
