42a7e474388029f3f33129c40d03c3b09129d02e
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-30 17:14:52 +0700

    Update CreateAzureAppGraphPowerShell_ClientSecret.ps1

--- GraphPowerShell/CreateAzureAppGraphPowerShell_ClientSecret.ps1
+++ GraphPowerShell/CreateAzureAppGraphPowerShell_ClientSecret.ps1
@@ -47,9 +47,7 @@ Write-Host "Client Secret: $($clientSecret.SecretText)"
 #$ClientSecretPass = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
 
 #Create a credential object using the client ID and secure string
-#$ClientSecretCredential = New-Object `
-#    -TypeName System.Management.Automation.PSCredential `
-#   -ArgumentList $ClientId, $ClientSecretPass
+#$ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ClientId, $ClientSecretPass
 
 #Connect to Microsoft Graph with Client Secret
 #Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential
